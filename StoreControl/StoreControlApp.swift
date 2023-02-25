ee42b21b1fc0b6ed8c3c1a75f8c1f37faaa4daef
swifticul <swifticul@icloud.com>
2023-02-25 09:39:11 +0100

    Initial Commit

---
+++ StoreControl/StoreControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreControlApp.swift
+//  StoreControl
+//
+//  Created by Tristan on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
