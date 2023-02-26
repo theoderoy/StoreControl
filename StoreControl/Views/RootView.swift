0a92c79234d371ff7c6038b7bac870567501a51a
swifticul <swifticul@icloud.com>
2023-02-26 11:20:12 +0100

    Add DemoLoop files

--- StoreControl/Views/RootView.swift
+++ StoreControl/Views/RootView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("DemoLoop Enabled?") private var demoloopon = false
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     var body: some View {
         NavigationView {
             List {
                 Section {
-                    Text("Version 1.0.0 - MDC and TCCD exploits by Ian Beer & zhuowei")
+                    Text("Version \(appVersion ?? "AppVersion") - MDC and TCCD exploits by Ian Beer & zhuowei")
                 }
                 Section {
                     NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
@@ -22,11 +23,9 @@ struct ContentView: View {
                 } footer: {
                     Text("You might need to approve the sandbox escape if this is the first time you're running StoreControl.")
                 }
-                Section {
-                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                        Text("Change DemoLoop Icon")
-                    } .disabled(demoloopon == false)
-                }
+                Button("Change DemoLoop Icon") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                } .disabled(demoloopon == false)
             } .navigationTitle("StoreControl")
         }
     }
