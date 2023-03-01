530d0957bbe412d95db0a73eeda1bbc3e6f38d2d
swifticul <swifticul@icloud.com>
2023-03-01 15:44:46 +0100

    Work on automatically fetching App ID (hideous code rn )

--- StoreControl/Views/DemoLoopSurgeryAdd.swift
+++ StoreControl/Views/DemoLoopSurgeryAdd.swift
@@ -43,15 +43,18 @@ struct DemoLoopSurgery: View {
                 .font(.largeTitle.weight(.bold))
                 .multilineTextAlignment(.center)
                 .frame(width: 350)
-            ProgressBar(progress: $progress)
-                            .frame(width: 300, height: 20)
+            VStack(spacing: 10) {
+                Text("Starting Bundle ID search")
+                ProgressBar(progress: $progress)
+                                .frame(width: 300, height: 20)
+            }
             Text("By clicking Enable DemoLoop, StoreControl will attempt to repair DemoLoop by restoring the neccesary resources it does not normally come with.")
                 .font(.subheadline.weight(.regular))
                 .frame(width: 340)
                 .clipped()
                 .multilineTextAlignment(.center)
             Button("\(ButtonText)") {
-                SurgeryAdd()
+                searchForBundleID(in: "/private/var/mobile/Containers/Data/Application")
                 EnablePerformButton = false
             } .disabled(EnablePerformButton == false) .buttonStyle(ButtonFromInteractfulROFL()) .frame(width: 350)
         }
@@ -71,6 +74,7 @@ struct DemoLoopSurgery: View {
                 if fileManager.fileExists(atPath: destinationPath) {
                     try fileManager.removeItem(atPath: destinationPath)
                     print("Existing folder removed successfully or was not found!")
+                    self.progress = 0.8
                 }
                 try fileManager.copyItem(atPath: originalPath, toPath: destinationPath)
                 print("Folder copied successfully!")
@@ -92,6 +96,7 @@ struct DemoLoopSurgery: View {
     }
     func searchForFolderName() -> String? {
         let fileManager = FileManager.default
+        
         let appDirectory = "/private/var/mobile/Containers/Data/Application/"
         
         do {
@@ -102,7 +107,7 @@ struct DemoLoopSurgery: View {
                 let metadataData = try Data(contentsOf: URL(fileURLWithPath: metadataFilePath))
                 let metadataPlist = try PropertyListSerialization.propertyList(from: metadataData, format: nil) as? [String: Any]
                 
-                if let bundleId = metadataPlist?["MCMMetadataIdentifier"] as? String, bundleId == "com.apple.ist.demoloop" {
+                if let bundleId = metadataPlist?["MCMMetadataIdentifier"] as? String, bundleId == "" {
                     return folderName
                 }
             }
@@ -111,6 +116,36 @@ struct DemoLoopSurgery: View {
         }
         
         return nil
+    }
+    func searchForBundleID(in directory: String) {
+        do {
+            let fileManager = FileManager.default
+            let contents = try fileManager.contentsOfDirectory(atPath: directory)
+            for content in contents {
+                let fullPath = directory + "/" + content
+                print("Searching in directory: \(fullPath)")
+                var isDir: ObjCBool = false
+                if fileManager.fileExists(atPath: fullPath, isDirectory: &isDir) {
+                    if isDir.boolValue {
+                        // Recursively search subdirectories
+                        searchForBundleID(in: fullPath)
+                    } else {
+                        // Look for Info.plist files
+                        if content == "Info.plist" {
+                            if let infoDict = NSDictionary(contentsOfFile: fullPath) as? [String: Any],
+                                let bundleID = infoDict["CFBundleIdentifier"] as? String,
+                                let appName = infoDict["CFBundleName"] as? String,
+                                appName == "DemoLoop" {
+                                print("Found DemoLoop app with Bundle ID: \(bundleID)")
+                                EnablePerformButton = true
+                            }
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
     }
 }
 
